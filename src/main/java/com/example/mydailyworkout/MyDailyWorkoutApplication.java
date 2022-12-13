b4aeb3391fc89a8cb53ff5cf0be4b220c44dcc0c
Da-zeros <daniromanpt@gmail.com>
2022-12-13 22:02:48 +0100

    packages added

--- src/main/java/com/example/mydailyworkout/MyDailyWorkoutApplication.java
+++ src/main/java/com/example/mydailyworkout/MyDailyWorkoutApplication.java
@@ -2,8 +2,10 @@ package com.example.mydailyworkout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MyDailyWorkoutApplication {
 
     public static void main(String[] args) {
