d5e141f0292c6a538830c5b1ba898c67d159cba3
Da-zeros <daniromanpt@gmail.com>
2022-12-13 21:21:25 +0100

    project innit

---
+++ src/main/java/com/example/mydailyworkout/MyDailyWorkoutApplication.java
@@ -0,0 +1,13 @@
+package com.example.mydailyworkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDailyWorkoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDailyWorkoutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mydailyworkout/MyDailyWorkoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydailyworkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDailyWorkoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
